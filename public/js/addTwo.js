12d81bbaa9f4d16cac4b0d3f3674a535f57a9d6a
Thomas Dowling <thomas@thomas-Vostro-2520.(none)>
2014-03-25 13:25:36 +0000

    added modules successfully

--- public/js/addTwo.js
+++ public/js/addTwo.js
@@ -1,83 +1,85 @@
-(function() {
-var button = $$('myButton');
+var mymodule = (function () {
+//Private
 
-function $$(id) {
-    'use strict';
-    if (typeof id != 'undefined') {
-        return document.getElementById(id);
+//Public
+    return {
+
+        showDivs: function (arg) {
+        document.getElementById(arg).style.visibility = 'visible';
+        },
+
+        hideDivs: function (arg) {
+        document.getElementById(arg).style.visibility = 'hidden';
+    },
+
+        showBackground: function (arg) {
+        $('#' + arg).css('background-color', '#f7bebe');
+    },
+
+
+    $$: function (id) {
+        'use strict';
+        if (typeof id != 'undefined') {
+            return document.getElementById(id);
+        }
+        else {
+            return 'undefined'
+        }
+        },
+        addNumbers:  function () {
+
+        var fnum = parseInt(this.$$('firstNumber').value);
+        var snum = parseInt(this.$$('secondNumber').value);
+
+        if (isNaN(fnum) === false && isNaN(snum) === false) {
+            this.$$('output').innerHTML = fnum + snum;
+            this.showDivs('scorewrapper');
+            return false;
+        }
+
+        else if (isNaN(fnum) === true && isNaN(snum) === false) {
+            this.showBackground('firstNumber');
+            this.showDivs('firstNumberTip');
+            return false;
+        }
+
+        else if (isNaN(snum) === true && isNaN(fnum) === false) {
+            this.showBackground('secondNumber');
+            this.showDivs('secondNumberTip');
+            return false;
+        }
+
+        else {
+            this.showBackground('firstNumber');
+            this.showBackground('secondNumber');
+            this.showDivs('firstNumberTip');
+            this.showDivs('secondNumberTip');
+            return false;
+        }
     }
-    else {
-        return 'undefined'
     }
-}
+})();
 
-function addNumbers() {
-
-    var fnum = parseInt($$('firstNumber').value);
-    var snum = parseInt($$('secondNumber').value);
-
-    if (isNaN(fnum) === false && isNaN(snum) === false) {
-        $$('output').innerHTML = fnum + snum;
-        showDivs('scorewrapper');
-        return false;
-    }
-
-    else if (isNaN(fnum) === true && isNaN(snum) === false) {
-        showBackground('firstNumber');
-        showDivs('firstNumberTip');
-        return false;
-    }
-
-    else if (isNaN(snum) === true && isNaN(fnum) === false) {
-        showBackground('secondNumber');
-        showDivs('secondNumberTip');
-        return false;
-    }
-
-    else {
-        showBackground('firstNumber');
-        showBackground('secondNumber');
-        showDivs('firstNumberTip');
-        showDivs('secondNumberTip');
-        return false;
-    }
-}
-
-function showDivs(arg) {
-    $$(arg).style.visibility = 'visible';
-}
-
-function hideDivs(arg) {
-    $$(arg).style.visibility = 'hidden';
-}
-
-function showBackground(arg) {
-    $('#' + arg).css('background-color', '#f7bebe');
-}
+//jQuery
+$(document).ready(function () {
 
     $('#firstNumber').focus(function () {
         $('#firstNumber').css('background-color', '#fff');
-        hideDivs('firstNumberTip');
-        hideDivs('scorewrapper');
+        mymodule.hideDivs('firstNumberTip');
+        mymodule.hideDivs('scorewrapper');
     });
 
     $('#secondNumber').focus(function () {
         $('#secondNumber').css('background-color', '#fff');
-        hideDivs('secondNumberTip');
-        hideDivs('scorewrapper');
+        mymodule.hideDivs('secondNumberTip');
+        mymodule.hideDivs('scorewrapper');
     });
+});
 
-//    button.onclick = function () {
-//        addNumbers();
-//        $('#dragpara').hide().fadeIn(2000);
-//        return false;
-//
-//    };
 
-    button.addEventListener("click",function () {
-        addNumbers();
-        $('#dragpara').hide().fadeIn(2000);
-        return false;
-
-    }, false);
-})()
+//AddEventListener
+mymodule.$$('myButton').addEventListener("click",function () {
+    mymodule.addNumbers();
+    $('#dragpara').hide().fadeIn(2000);
+    return false;
+}, false);
